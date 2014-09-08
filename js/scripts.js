428fffe20025a12a4c689d35d5ef7d37cc2df123
fhandiedicion <fhandiedicion@gmail.com>
2014-09-08 19:55:14 +0200

    Mi Primer ejercicio de javaScript
    Definición de variables. Operaciones matemáticas básicas

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+/* comentario en bloque
+ndsbgrnhklmb vbm v ,m, nm nmn mcvmn mncvm*/
+
+//Comentario de linea
+
+//Prueba de funcionamiento de JS
+//document.write("Si funciona mi JavaScript");
+
+//Generar alerta a usuario
+//alert("Ventana de alerta para usuario");
+
+//Solicitar Información a nuestros usuarios
+/*var nombre = prompt("Por favor introduzca su nombre");
+document.write("<h1>Bienvenid@: " + nombre + "</h1><br/>");
+
+var edad = prompt("Por favor introduzca Edad");
+document.write("Su edad es: " + edad + " años");*/
+
+//Operaciones Matemáticas 
+/*var numUno = 5;
+var numDos = 2;
+var numTres = 2;
+
+var suma = numUno + numDos;
+
+var resultado = suma / numTres;
+
+console.log(suma);
+
+console.log(resultado);*/
+
+
+//Operaciones Matemáticas Personalizadas
+var numUno = parseInt(prompt("Introduzca Primer Valor"));
+var numDos = parseInt(prompt("Introduzca Segundo Valor"));
+var numTres = parseInt(prompt("Introduzca Tercer Valor"));
+
+var suma = (numUno + numDos) / numTres;
+
+var resultado = suma / numTres;
+
+console.log("La operación es la siguiente: " + numUno + " + " + numDos + " / " + numTres);
+
+console.log(resultado);
